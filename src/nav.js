56ce424aa1dfbbf2eae0f6067f8f0f3c56c4bd6b
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-09-07 08:54:18 +1000

    updated readme

---
+++ src/nav.js
@@ -0,0 +1,23 @@
+// This code is from stack overflow, found this was a good way to have a burger nav bar...
+ function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  
+  document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
+
+// ===== Scroll to Top ==== //  // ===== Using this piece of code to be able to back to top- this was learnt via a code pen ==== // 
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+      $('#return-to-top').fadeIn(200);    // Fade in the arrow
+  } else {
+      $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+  }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+  $('body,html').animate({
+      scrollTop : 0                       // Scroll to top of body
+  }, 500);
+});
+//******************************************************************************* */ // 
